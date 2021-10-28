caee86863b6933cb75d9ccabb48ce3dd28f4b5bf
claudine100 <ndayisabaclaudine01@gmail.com>
2021-10-28 19:00:43 +0200

    creation ArrayList

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ApplicationC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //ArrayList:La classe ArrayList est une collection 
+            //qui peut être utilisée pour tout type ou objet. 
+            //Arraylist est une classe similaire à un tableau,
+            ///mais elle peut être utilisée pour stocker 
+            ///des valeurs de différents types.
+            //Un Arraylist n’a pas de taille spécifique.
+            //N’importe quel nombre d’éléments peut être stocké.
+            ArrayList numbers = new ArrayList();  
+            numbers.Add(1);       //int
+            numbers.Add(1.9803);  //float
+            numbers.Add("one");   //string
+            numbers.Add('o');     //char
+                
+            foreach (object nbr in numbers)  
+            {  
+            Console.Write("{0} ", nbr);
+            }
+
+        }
+    }
+}
