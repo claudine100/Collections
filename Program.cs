7c82a57df089c3f12a64c443de460006ac65d128
claudine100 <ndayisabaclaudine01@gmail.com>
2021-10-28 19:19:16 +0200

    creation Stack

--- Program.cs
+++ Program.cs
@@ -22,12 +22,16 @@ namespace ApplicationC_
             numbers.Add(1.9803);  //float
             numbers.Add("one");   //string
             numbers.Add('o');     //char
-                
+                Console.WriteLine("POUR ARRAYLIST");
+             Console.WriteLine("-----------------");
             foreach (object nbr in numbers)  //ou  foreach (var nbr in numbers)  
             {  
             Console.Write("{0} ", nbr);
             }
              Console.WriteLine("\n");
+             Console.WriteLine("POUR LE HASHTABLE");
+             Console.WriteLine("-----------------");
+             
             //POUR   HASHTABLE 
             //HashTable est similaire à arraylist,
             // mais représente les éléments sous forme de clé et valeur.
@@ -41,6 +45,27 @@ namespace ApplicationC_
             {  
             Console.WriteLine("Clé: {0}, Valeur: {1}", language.Key, language.Value);   
             }
+            //POUR STACK
+            //Il représente une collection d’objets 
+            //LIFO(Last In First Out). Lorsque vous 
+            //ajoutez un élément dans la liste, cela 
+            //s’appelle PUSH ou Empiler et lorsque vous
+            // le supprimez, cela s’appelle POP ou Dépiler.
+            Console.WriteLine("\n");
+             Console.WriteLine("POUR LE STACK");
+             Console.WriteLine("-------------");
+             Stack languages = new Stack();  
+
+            languages.Push("Java");  
+            languages.Push("C#");  
+            languages.Push("Python");  
+            languages.Push("C++");  
+
+            foreach (object o in languages)  
+            {
+            Console.WriteLine(o);
+            }
+
         }
     }
 }
