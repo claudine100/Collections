9cd741be24a515ebab211dfa093340d797104d76
claudine100 <ndayisabaclaudine01@gmail.com>
2021-10-28 19:12:14 +0200

    creation HashTable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ApplicationC_
 {
@@ -7,6 +8,8 @@ namespace ApplicationC_
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //POUR ARRAYLIST
+
             //ArrayList:La classe ArrayList est une collection 
             //qui peut être utilisée pour tout type ou objet. 
             //Arraylist est une classe similaire à un tableau,
@@ -20,11 +23,24 @@ namespace ApplicationC_
             numbers.Add("one");   //string
             numbers.Add('o');     //char
                 
-            foreach (object nbr in numbers)  
+            foreach (object nbr in numbers)  //ou  foreach (var nbr in numbers)  
             {  
             Console.Write("{0} ", nbr);
             }
-
+             Console.WriteLine("\n");
+            //POUR   HASHTABLE 
+            //HashTable est similaire à arraylist,
+            // mais représente les éléments sous forme de clé et valeur.
+            Hashtable languages = new Hashtable();  
+            languages.Add(1, "Java");  
+            languages.Add(2, "C#");  
+            languages.Add(3, "Python");  
+            languages.Add(4, "C++");  
+            
+            foreach (DictionaryEntry language in languages)  
+            {  
+            Console.WriteLine("Clé: {0}, Valeur: {1}", language.Key, language.Value);   
+            }
         }
     }
 }
